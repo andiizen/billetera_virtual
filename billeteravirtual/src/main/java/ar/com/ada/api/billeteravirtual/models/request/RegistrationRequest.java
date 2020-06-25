d7050e70f7aa981870ccaf8a21627a498fd49fc7
andiizen <andiizenklusen@hotmail.com>
2020-06-25 16:22:33 -0300

    Se agrega el LoginRequest y RegistrationRequest

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/models/request/RegistrationRequest.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/models/request/RegistrationRequest.java
@@ -1,5 +1,18 @@
 package ar.com.ada.api.billeteravirtual.models.request;
 
+import java.util.Date;
+
+/**
+ * RegistrationRequest
+ */
 public class RegistrationRequest {
+
+    public String fullName; //Nombre persona
+    public int country; //pais del usuario
+    public int identificationType; //Tipo Documento
+    public String identification; //nro documento
+    public Date birthDate; //fechaNacimiento
+    public String email; //email
+    public String password; //contraseña elegida por el usuario.
     
 }
