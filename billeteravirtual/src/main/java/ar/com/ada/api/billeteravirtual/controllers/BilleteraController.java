0d0308554786c234ae702460fbaf3de53f3e168c
andiizen <andiizenklusen@hotmail.com>
2020-07-01 11:23:31 -0300

    Se crean dos web methods consultarSaldo (con sobrecarga de operadores para realizar un GET que puede devolver todos los saldos que tienen las cuentas de una billetera o el saldo de una cuenta segun la moneda.
    En el BilleteraService se crean los metodos consultarSaldo, que te devuelve un saldo segun billeteraId y moneda, y buscarPorId que devuelve una billetera segun el id.
    También se crea un objeto SaldoResponse que se ultiliza para devolver un json a front con las variables moneda y saldo.

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
@@ -1,9 +1,65 @@
 package ar.com.ada.api.billeteravirtual.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.billeteravirtual.entities.Billetera;
+import ar.com.ada.api.billeteravirtual.entities.Cuenta;
+import ar.com.ada.api.billeteravirtual.models.response.*;
+import ar.com.ada.api.billeteravirtual.services.BilleteraService;
 
 @RestController
 public class BilleteraController {
+
+    @Autowired
+    BilleteraService billeteraService;
+
+    /*
+     * webMetodo 1: consultar saldo: GET URL:/billeteras/{id}/saldos/{moneda}
+     * webMetodo 2: cargar saldo: POST URL:/billeteras/{id}/recargas requestBody: {
+     * "moneda": "importe": } webMetodo 3:
+     * 
+     * enviar saldo: POST URL:/billetera/{id}/envios requestBody: { "moneda":
+     * "importe": "email": "motivo": "detalleDelMotivo": }
+     */
+
+    @GetMapping("/billeteras/{id}/saldos/{moneda}")
+    public ResponseEntity<?> consultarSaldo(@PathVariable Integer id, @PathVariable String moneda) {
+
+        SaldoResponse saldo = new SaldoResponse();
+
+        saldo.saldo = billeteraService.consultarSaldo(id, moneda);
+        saldo.moneda = moneda;
+
+        return ResponseEntity.ok(saldo);
+    }
+
+    @GetMapping("/billeteras/{id}/saldos")
+    public ResponseEntity<List<SaldoResponse>> consultarSaldo(@PathVariable Integer id) {
+
+        Billetera billetera = new Billetera();
+
+        billetera = billeteraService.buscarPorId(id);
+
+        List<SaldoResponse> saldos = new ArrayList<>();
+
+        for (Cuenta cuenta : billetera.getCuentas()) {
+
+            SaldoResponse saldo = new SaldoResponse();
+
+            saldo.saldo = cuenta.getSaldo();
+            saldo.moneda = cuenta.getMoneda();
+            saldos.add(saldo);
+        }
+        return ResponseEntity.ok(saldos);
+    }
+}
 
     /*
     * webMetodo 1:
@@ -30,4 +86,3 @@ public class BilleteraController {
                 "detalleDelMotivo":
             }
     */
-}
--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/BilleteraService.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/BilleteraService.java
@@ -7,44 +7,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.com.ada.api.billeteravirtual.entities.*;
-import ar.com.ada.api.billeteravirtual.repos.*;
+import ar.com.ada.api.billeteravirtual.repos.BilleteraRepository;
 
 @Service
 public class BilleteraService {
 
     @Autowired
-    BilleteraRepository billeteraRepository;
+    BilleteraRepository repo;
 
-    public void grabar(Billetera billetera) {
-        billeteraRepository.save(billetera);
+    /* 2. Metodo: enviar plata
+    2.1-- recibir un importe, la moneda en la que va a estar ese importe
+    recibir una billetera de origen y otra de destino
+    2.2-- actualizar los saldos de las cuentas (a una se le suma y a la otra se le resta)
+    2.3-- generar dos transacciones
+    */
+
+    /* 3. Metodo: consultar saldo 
+    3.1-- recibir el id de la billetera y la moneda en la que esta la cuenta
+    */
+
+    public void grabar(Billetera billetera){
+        repo.save(billetera);
     }
 
-    /**
-     * 1.Metodo: cargar saldo. 1.1 Recibir un importe. Se busca una billetera por ID
-     * y una cuenta por la moneda. 1.2 hacer transaccion 1.3 actualizar saldo de la
-     * billetera
-     *
-     */
-    public void cargarSaldo(BigDecimal saldo, String moneda, Integer billeteraId, String conceptoOperacion,
-            String detalle) {
+    public void cargarSaldo(BigDecimal saldo, String moneda, Integer billeteraId, 
+    String conceptoOperacion, String detalle){
 
-        Billetera billetera = billeteraRepository.findByBilleteraId(billeteraId);
+        Billetera billetera = repo.findByBilleteraId(billeteraId);
 
         Cuenta cuenta = billetera.getCuenta(moneda);
 
         Transaccion transaccion = new Transaccion();
-        // transaccion.setCuenta(cuenta);
         transaccion.setMoneda(moneda);
         transaccion.setFecha(new Date());
         transaccion.setConceptoOperacion(conceptoOperacion);
         transaccion.setDetalle(detalle);
         transaccion.setImporte(saldo);
-        transaccion.setTipoOperacion(1); // 1 = acreditar(entrada). 2 = debitar(salida)
-        transaccion.setEstadoId(1); // 1 = aprobada . 0= pendiente -1 =rechazada
+        transaccion.setTipoOperacion(1);// 1 Entrada, 0 Salida
+        transaccion.setEstadoId(1);// -1 Rechazada 0 Pendiente 1 Aprobada
         transaccion.setDeCuentaId(cuenta.getCuentaId());
-        transaccion.setaCuentaId(cuenta.getCuentaId());
         transaccion.setDeUsuarioId(billetera.getPersona().getUsuario().getUsuarioId());
         transaccion.setaUsuarioId(billetera.getPersona().getUsuario().getUsuarioId());
+        transaccion.setaCuentaId(cuenta.getCuentaId());
 
         cuenta.agregarTransaccion(transaccion);
 
@@ -53,17 +57,20 @@ public class BilleteraService {
         cuenta.setSaldo(saldoNuevo);
 
         this.grabar(billetera);
+    }
+
+    public BigDecimal consultarSaldo (Integer billeteraId, String moneda){
+
+        Billetera billetera = repo.findByBilleteraId(billeteraId);
+
+        Cuenta cuenta = billetera.getCuenta(moneda);
+
+        return cuenta.getSaldo();
 
     }
 
-    /*
-     * 2. Metodo: enviar dinero a otra cuenta. 2.1 recibir: (parametros) un importe.
-     * Moneda en la que va a estar ese importe. Billetera origen a billetera de
-     * destino (usuario desde hasta, o mail a mail). 2.2 Actualizar saldos de las
-     * cuentas(suma y resta) 2.3 Generar dos transacciones
-     * 
-     * 3. Metodo: Consultar Saldo 3.1 Recibir id de billetera y id de moneda.
-     * 
-     * 
-     */
+    public Billetera buscarPorId(Integer id){
+
+        return repo.findByBilleteraId(id);
+    }
 }
